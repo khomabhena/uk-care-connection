394045d47b33de86daa5a940cd730298ff22b8fb
khomabhena <colwanymab@gmail.com>
2023-01-04 15:26:35 +0200

    Global variables for the Navbar Details

--- src/components/Dashboard/NavbarDetails/index.js
+++ src/components/Dashboard/NavbarDetails/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Container, Image, Name } from './NavbarDetailsElements'
-import person from '../../../images/person.jpg'
 
 const NavbarDetails = ({children, image, name}) => {
   return (
     <Container>
-        <Image src={person} />
-        <Name>Emily Burner</Name>
+        <Image src={image} />
+        <Name>{name}</Name>
         {children}
     </Container>
   )
